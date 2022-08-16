8921b6f1775aa99fd18a1986e1b063cd6b67461e
Dayana <daanasozinova6@gmail.com>
2022-08-16 19:20:38 +0500

    fix

--- logistic/serializers.py
+++ logistic/serializers.py
@@ -13,7 +13,6 @@ class ProductPositionSerializer(serializers.ModelSerializer):
     class Meta:
         model = StockProduct
         fields = ['id', 'product', 'quantity', 'price']
-        # extra_kwargs = {'stock': {'required': 'False'}}
 
 
 class StockSerializer(serializers.ModelSerializer):
@@ -34,24 +33,12 @@ class StockSerializer(serializers.ModelSerializer):
         return stock
 
     def update(self, instance, validated_data):
-        # достаем связанные данные для других таблиц
+
         positions = validated_data.pop('positions')
-        # обновляем склад по его параметрам
+
         stock = super().update(instance, validated_data)
-        for a, position in positions:
-            position_obj = StockProduct.objects.update(stock=stock, **position)
-            stock.positions.add(position_obj)
-        # здесь вам надо обновить связанные таблицы
-        # в нашем случае: таблицу StockProduct
-        # с помощью списка positions
+        for position in positions:
+            StockProduct.objects.update_or_create(stock=stock, product=position['product'],
+                                                  defaults={'price': position['price'], 'quantity': position['quantity']})
 
         return stock
-
-    # def update_or_create(self, instance, validated_data):
-    #     positions = validated_data.pop('positions')
-    #     stock = super().update_or_create(instance, validated_data)
-    #     for position in positions:
-    #         position_obj = StockProduct.objects.update_or_create(stock=stock, **position)
-    #         stock.positions.add(position_obj)
-    #
-    #     return stock
